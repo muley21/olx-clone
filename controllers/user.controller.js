be089cfa5c87c14cf702fffb8f6f049a481608aa
Neha Muley <nehamuley955@gmail.com>
2024-07-26 16:20:29 +0530

    email otp

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,7 +4,7 @@ const sendEmail = require("../utils/email")
 exports.verifyUserEmail = asyncHandler(async (req, res) => {
     const result = await User.findById(req.loggedInUser)
     const otp = Math.floor(10000 + Math.random() * 900000)
-    await User.findByIdAndUpdate(req.loggedInUser, { emailCode: `<h1>${otp}</h1>` })
+    await User.findByIdAndUpdate(req.loggedInUser, { emailCode: otp })
     await sendEmail({ to: result.email, subject: "Verify Email", message: `Your Otp is ${otp}` })
 
     res.json({ message: "Verfiication Send Success" })
