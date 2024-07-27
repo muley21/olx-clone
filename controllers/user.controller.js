c054ae58c6c08c3c9a5e6c342d14d66135dc246b
Neha Muley <nehamuley955@gmail.com>
2024-07-27 13:12:07 +0530

    mobile verifivation

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,9 +1,13 @@
 const asyncHandler = require("express-async-handler")
 const User = require("../models/User")
 const sendEmail = require("../utils/email")
+
 exports.verifyUserEmail = asyncHandler(async (req, res) => {
     console.log(req.loggedInUser)
     const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You Are Not Logged In..Please Login Again" })
+    }
     const otp = Math.floor(10000 + Math.random() * 900000)
     await User.findByIdAndUpdate(req.loggedInUser, { emailCode: otp })
     await sendEmail({ to: result.email, subject: "Verify Email", message: `Your Otp is ${otp}` })
@@ -15,9 +19,25 @@ exports.verifyEmailOTP = asyncHandler(async (req, res) => {
 
     const { otp } = req.body
     const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You Are Not Logged In..Please Login Again" })
+    }
     if (otp !== result.emailCode) {
         return res.status(400).json({ message: "Inavlid Otp" })
     }
     await User.findByIdAndUpdate(req.loggedInUser, { emailVerified: true })
     res.json({ message: "Email Verify Success" })
+})
+exports.verifyMobileOTP = asyncHandler(async (req, res) => {
+
+    const { otp } = req.body
+    const result = await User.findById(req.loggedInUser)
+    if (!result) {
+        return res.status(401).json({ message: "You Are Not Logged In..Please Login Again" })
+    }
+    if (otp !== result.mobileCode) {
+        return res.status(400).json({ message: "Inavlid Otp" })
+    }
+    await User.findByIdAndUpdate(req.loggedInUser, { mobileVerified: true })
+    res.json({ message: "mobile Verify Success" })
 })
