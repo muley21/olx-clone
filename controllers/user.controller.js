2e50caf4190b489d02113cdd8b1b32bf2b07650b
Neha Muley <nehamuley955@gmail.com>
2024-07-31 15:49:08 +0530

    databse entry

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -109,6 +109,6 @@ exports.addPost = asyncHandler(async (req, res) => {
 
     // modify this code to support cloudinary
 
-    // await Posts.create({ title, desc, price, images, location, category, user: req.loggedInUser })
+    await Posts.create({ title, desc, price, images, location, category, user: req.loggedInUser })
     return res.json({ message: "Post Create Success" })
 })
